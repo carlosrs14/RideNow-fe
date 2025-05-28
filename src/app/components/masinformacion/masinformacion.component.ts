08eb6fcdde6899ca9a78f7b484889a43ef05873b
carlosrs14 <crincones578@gmail.com>
2025-05-28 08:12:53 -0500

    mas informacion del prestador de servicio

--- src/app/components/masinformacion/masinformacion.component.ts
+++ src/app/components/masinformacion/masinformacion.component.ts
@@ -1,6 +1,10 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { Review } from '../../models/Review';
+import { UserService } from '../../services/user/user.service';
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Usuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-masinformacion',
@@ -9,8 +13,35 @@ import { Review } from '../../models/Review';
   styleUrl: './masinformacion.component.css'
 })
 export class MasinformacionComponent {
-  nombre: string = "";
-  correo: string = "";
-  reviews: Review[] = [];
+    usuario: Usuario = new Usuario(0, "", "", "", "", new Date(), "", "");
+    reviews: Review[] = [];
+
+	constructor(private servicioUsuario: UserService, private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		const idViajeStr = this.route.snapshot.paramMap.get("id");
+		if (!idViajeStr) return;
+		const idViaje: number = parseInt(idViajeStr);
+
+		this.servicioUsuario.getByIdViaje(idViaje).subscribe({
+			next: (response) => {
+				this.usuario = new Usuario(
+					response.id,
+					response.nombre,
+					response.apellido,
+					response.tipo,
+					response.correo,
+					response.fechaNacimiento,
+					response.telefono,
+					""
+				)
+			},
+			error: (error) => {
+				console.log(error);
+				Swal.fire("Error", "No se pudo obtener la informacion", "error");
+			}
+		});
+
+	}
 
 }
