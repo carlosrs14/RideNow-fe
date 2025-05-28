3847de4ebb5c61fcd587f85f246119a741d6e223
carlosrs14 <crincones578@gmail.com>
2025-05-28 09:40:03 -0500

    que la parte de denuncias solo se le muestre al que es cliente

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-navbar',
-	imports: [RouterLink],
+	imports: [RouterLink, NgIf],
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 	
 	constructor(private router: Router) {}
+	tipoUsuario: string = "";
+	ngOnInit() {
+		const tipo = localStorage.getItem('tipoUsuario')
+		if (tipo) this.tipoUsuario = tipo;
+	}
 	
 	login() {
 		if (localStorage.getItem("idUsuario")) {
